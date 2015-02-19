642723d23a51238177d3ffb5ab9ed7731ff323bc
czengg <czengg@CMU-896368.WV.CC.CMU.EDU>
2015-02-18 21:03:49 -0500

    website skeleton done

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    easing: "swing",
+    direction: "horizontal",
+    directionNav: true,
+    prevText: "<",
+    nextText: ">"
+  });
+}); 
